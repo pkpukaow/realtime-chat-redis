ce42bf538b054180445211cb9ba54d82cc86e58f
ratchapon lovitayanon <ratchapon@aisoftthailand.com>
2025-12-11 17:41:32 +0700

    first commit

---
+++ src/app/api/[[...slugs]]/route.ts
@@ -0,0 +1,50 @@
+import { authMiddleware } from "@/app/api/[[...slugs]]/auth";
+import { redis } from "@/lib/redis";
+import { Elysia } from "elysia";
+import { nanoid } from "nanoid";
+import { z } from "zod";
+
+const ROOM_TTL_SECONDS = 60 * 10;
+
+const rooms = new Elysia({ prefix: "/room" }).post("/create", async () => {
+  const roomId = nanoid();
+
+  await redis.hset(`meta:${roomId}`, {
+    connected: [],
+    createdAt: Date.now(),
+  });
+
+  await redis.expire(`meta:${roomId}`, ROOM_TTL_SECONDS);
+
+  return { roomId };
+});
+
+const messages = new Elysia({ prefix: "/messages" }).use(authMiddleware).post(
+  "/",
+  async ({ body, auth }) => {
+    const { sender, text } = body;
+    const { roomId } = auth;
+
+    const roomExists = await redis.exists(`meta:${roomId}`);
+
+    if (!roomExists) {
+      throw new Error("Room not found");
+    }
+  },
+  {
+    query: z.object({
+      roomId: z.string(),
+    }),
+    body: z.object({
+      sender: z.string().max(100),
+      text: z.string().max(1000),
+    }),
+  }
+);
+
+const app = new Elysia({ prefix: "/api" }).use(rooms);
+
+export const GET = app.fetch;
+export const POST = app.fetch;
+
+export type App = typeof app;
